2d156992027f258a69d703b29d3f09327d0d5419
dritzii <phammyjohn@hotmail.com>
2024-09-12 10:17:24 +1000

    init

--- automated_tcm/automated_tcm_stack.py
+++ automated_tcm/automated_tcm_stack.py
@@ -2,7 +2,7 @@ from aws_cdk import (
     Stack,
     aws_codebuild as codebuild,
     aws_codepipeline as codepipeline,
-    aws_codepipeline_actions as pipeline_actions,
+    aws_codepipeline_actions as codepipeline_actions,
     aws_s3 as s3,
     aws_kms as kms,
     CfnParameter,
@@ -17,7 +17,7 @@ class AutomatedTcmStack(Stack):
 
         s3_bucket_location = CfnParameter(self, "s3bucket_name", default="ato-dis-infra-build-pipeline-outputs-devtest")
         source_bucket = s3.Bucket.from_bucket_name(self, 'test-pipeline-src-bucket', s3_bucket_location.value_as_string)
-        zip_code = CfnParameter(self, "lambdazip", default="approval.zip")
+        BucketKeyParam = CfnParameter(self, "lambdazip", default="approval.zip")
         kms_alias = CfnParameter(self, "kms_alias", default="alias/KMS-DIS-DockerImageBuilder")
 
         admin_role = CfnParameter(self, "admin_cb_role",
@@ -31,9 +31,10 @@ class AutomatedTcmStack(Stack):
                                                       encryption_key=kms.Alias.from_alias_name(self,
                                                                                                id='kms-key-s3bucket',
                                                                                                alias_name=kms_alias.value_as_string))
-
-        pipeline = codepipeline.Pipeline(self, 'dis-ram-pipe-testiac',
-                                         pipeline_name='dis-ram-pipe-testiac',
+        artifact = codepipeline.Artifact("buildspec_artifact")
+        # codepipeline
+        pipeline = codepipeline.Pipeline(self, 'dis-automated_tcm-mygovid',
+                                         pipeline_name='dis-mygov-automated-tcm',
                                          role=admin_role_arn,
                                          artifact_bucket=kms_bucket,
                                          cross_account_keys=False,
@@ -42,16 +43,31 @@ class AutomatedTcmStack(Stack):
                                          restart_execution_on_update=False,
                                          # pipeline_type=codepipeline.PipelineProps.V1
                                          )
+        pipeline.add_stage(stage_name='Source', actions=[codepipeline_actions.S3SourceAction(
+            action_name='S3Source',
+            bucket=kms_bucket,
+            bucket_key=BucketKeyParam.value_as_string,
+            output=artifact,
+            role=admin_role_arn,
+            trigger=codepipeline_actions.S3Trigger.POLL)])
 
-        # we pull git repo from ADO mygovid to run unit tests
-        codebuild.PipelineProject(self, "DIS-automated-tcm",
-                          build_spec=codebuild.BuildSpec.from_object({
-                              "version": "0.2",
-                              "phases": {
-                                  "build": {
-                                      "commands": ["echo \"Hello, CodeBuild!\""
-                                                   ]
-                                  }
-                              }
-                          })
-                          )
+        # init a cb project , we add the details here for the buildspec
+        project = codebuild.PipelineProject(self, "DIS-automated-TCM",
+                                            role=admin_role_arn,
+                                            encryption_key=kms.Alias.from_alias_name(self,
+                                                                     "codepipelinekmskeyalias",
+                                                                     alias_name=kms_alias.value_as_string),
+                                            project_name="dis-automated-tcm",
+                                            build_spec=codebuild.BuildSpec.from_source_filename(filename=""))
+
+        build_action = codepipeline_actions.CodeBuildAction(
+            action_name="CodeBuild",
+            project=project,
+            input=artifact,
+            #outputs=[codepipeline.Artifact("automated-tcm")],
+            execute_batch_build=True,
+            combine_batch_build_artifacts=True,
+            type=codepipeline_actions.CodeBuildActionType.TEST
+        )
+
+        pipeline.add_stage(stage_name='build', actions=[build_action])
