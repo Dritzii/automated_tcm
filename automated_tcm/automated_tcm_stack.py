f8a9055017497a4a3ccf8d0998a352107162d7d3
dritzii <phammyjohn@hotmail.com>
2024-10-14 13:34:50 +1100

    init

--- automated_tcm/automated_tcm_stack.py
+++ automated_tcm/automated_tcm_stack.py
@@ -5,7 +5,10 @@ from aws_cdk import (
     aws_codepipeline_actions as codepipeline_actions,
     aws_s3 as s3,
     aws_kms as kms,
+    aws_lambda as _lambda,
     CfnParameter,
+    Duration,
+    aws_ec2
 )
 from aws_cdk.aws_iam import Role
 from constructs import Construct
@@ -19,15 +22,72 @@ class AutomatedTcmStack(Stack):
         source_bucket = s3.Bucket.from_bucket_name(self, 'test-pipeline-src-bucket', s3_bucket_location.value_as_string)
         bucket_key_param = CfnParameter(self, "vssln.zip", default="vssln.zip")
         kms_alias = CfnParameter(self, "kms_alias", default="alias/KMS-DIS-DockerImageBuilder")
+        existing_role_arn = CfnParameter(self, "lambda_role",
+                                         default="arn:aws:iam::830486629506:role/ato-role-dis-testiac-lambda")
+        existing_role = Role.from_role_arn(self, "ato-dis-stack_details_lambda", existing_role_arn.value_as_string,
+                                           mutable=False)
         admin_role = CfnParameter(self, "admin_cb_role")
         admin_role_arn = Role.from_role_arn(self, "ato-dis-cp-role", admin_role.value_as_string, mutable=False)
-
+        jinja = CfnParameter(self, "jinja", default="jinja.zip")
+        matplotlib = CfnParameter(self, "matplotlib", default="matplotlib.zip")
+        s3_bucket_location_reports = CfnParameter(self, "s3bucket_name_reports",
+                                                  default="ato-dis-infra-build-pipeline-outputs-devtest")
+        zip_code = CfnParameter(self, "lambdazip", default="approval.zip")
+        subnet = CfnParameter(self, "subnet_list", default="['subnet-0fb54d79' , 'subnet-0c03b384cc7d98682']")
+        vpcid = CfnParameter(self, 'vpc-6bcfd80e')
+        canarysg = CfnParameter(self, 'sg-0932df5016b614b95')
+        SGobj = [aws_ec2.SecurityGroup.from_security_group_id(self, 'sg', security_group_id=canarysg.value_as_string)]
+        Vpcobj = aws_ec2.Vpc.from_vpc_attributes(self, 'vpc', availability_zones=['ap-southeast-2a', 'ap-southeast-2b'],
+                                                 vpc_id=vpcid.value_as_string)
+        Subnetobj = aws_ec2.SubnetSelection(subnets=[aws_ec2.Subnet.from_subnet_id(self, "lambda-subnet0",
+                                                                                   subnet.value_as_string.split()[0]),
+                                                     aws_ec2.Subnet.from_subnet_id(self, "lambda-subnet1",
+                                                                                   subnet.value_as_string.split()[1])])
         kms_bucket = s3.Bucket.from_bucket_attributes(self,
                                                       id="ato-dis-kms-bucket",
                                                       bucket_name=source_bucket.bucket_name,
                                                       encryption_key=kms.Alias.from_alias_name(self,
                                                                                                id='kms-key-s3bucket',
                                                                                                alias_name=kms_alias.value_as_string))
+        # Layers
+        self.jinja_layer = _lambda.LayerVersion(self, "DIS-jinjaLayer",
+                                                code=_lambda.Code.from_bucket(bucket=kms_bucket,
+                                                                              key=jinja.value_as_string),
+                                                compatible_runtimes=[_lambda.Runtime.PYTHON_3_11,
+                                                                     _lambda.Runtime.PYTHON_3_10,
+                                                                     _lambda.Runtime.PYTHON_3_9,
+                                                                     _lambda.Runtime.PYTHON_3_8])
+        self.matplot_lib_layer = _lambda.LayerVersion(self, "DIS-matplotlibLayer",
+                                                      code=_lambda.Code.from_bucket(bucket=kms_bucket,
+                                                                                    key=matplotlib.value_as_string),
+                                                      compatible_runtimes=[_lambda.Runtime.PYTHON_3_11,
+                                                                           _lambda.Runtime.PYTHON_3_10,
+                                                                           _lambda.Runtime.PYTHON_3_9,
+                                                                           _lambda.Runtime.PYTHON_3_8])
+
+        # Lambda
+        TestFrameworkLambda_generate_html = _lambda.Function(self, "ato-dis-generate_report",
+                                                             runtime=_lambda.Runtime.PYTHON_3_11,
+                                                             handler="report_handler.handler",
+                                                             code=_lambda.Code.from_bucket(bucket=kms_bucket,
+                                                                                           key=zip_code.value_as_string),
+                                                             environment={
+                                                                 "s3_bucket": s3_bucket_location.value_as_string,
+                                                                 "s3_bucket_reports": s3_bucket_location_reports.value_as_string,
+                                                                 "config": "config.json",
+                                                                 "ARTIFACTORY_SVC_USER": "udfh8",
+                                                                 "ARTIFACTORY_SVC_USER_TOKEN": "cmVmdGtuOjAxOjE3NTAxMjY4NDY6S2M2RUZzTkx5TndsZUN1azR1cms0TlpIeU0x",
+                                                                 "ARTIFACTORY_ENDPOINT": "https://artifactory.ctz.atocnet.gov.au",
+                                                                 "ARTIFACTORY_REPO_STORAGE_PATH": "/artifactory/disolution-generic-release-local/AWS/Reports/"
+                                                             },
+                                                             timeout=Duration.minutes(10),
+                                                             memory_size=10240,
+                                                             role=existing_role,
+                                                             layers=[self.jinja_layer, self.matplot_lib_layer],
+                                                             vpc=Vpcobj,
+                                                             vpc_subnets=Subnetobj,
+                                                             security_groups=SGobj,
+                                                             )
 
         artifact = codepipeline.Artifact("buildspec_artifact")
         tcm = codepipeline.Artifact("automated-tcm")
@@ -70,3 +130,10 @@ class AutomatedTcmStack(Stack):
         )
 
         pipeline.add_stage(stage_name='build_test', actions=[build_action])
+        pipeline.add_stage(stage_name='build_test', actions=[
+            codepipeline_actions.LambdaInvokeAction(
+                action_name="GenerateReport",
+                lambda_=TestFrameworkLambda_generate_html,
+                role=admin_role_arn,
+                run_order=1,
+            ),])
---
+++ lambda/report_handler.py
@@ -0,0 +1,48 @@
+import base64
+import json
+import os
+
+import boto3
+
+def handler(event, context):
+    import urllib.request
+    from urllib.error import URLError, HTTPError
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+    codepipeline = boto3.client('codepipeline')
+    job_id = event['CodePipeline.job']['id']
+    data = get_file_context()
+    file_content = json.loads(data)
+    try:
+        graph_io = create_graph(file_content['piechart_context']).create_pie()
+        file_content['overallpiechart'] = base64.b64encode(graph_io).decode()
+    except Exception:
+        file_content['overallpiechart'] = False
+    # we should do our calculations and stuff before we render below this
+    html_str = render_html_template(file_content)
+    report_name = 'report_generation_' + str(datetime_int()) + '.html'
+    put_file_contents(html_str, report_name)
+    # Encode the username and password
+    credentials = f"{os.environ['ARTIFACTORY_SVC_USER']}:{os.environ['ARTIFACTORY_SVC_USER_TOKEN']}"
+    encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
+    request = urllib.request.Request(
+        os.environ['ARTIFACTORY_ENDPOINT'] + os.environ['ARTIFACTORY_REPO_STORAGE_PATH'] + report_name,
+        data=str.encode(html_str),
+        method='PUT')
+    request.add_header('Authorization', f'Basic {encoded_credentials}')
+    try:
+        with urllib.request.urlopen(request) as response:
+            response_data = response.read()
+            print(response.getcode())
+            print(response_data.decode('utf-8'))
+    except HTTPError as e:
+        print(f'HTTPError: {e.code} - {e.reason}')
+        codepipeline.put_job_failure_result(jobId=job_id)
+    except URLError as e:
+        print(f'URLError: {e.reason}')
+        codepipeline.put_job_failure_result(jobId=job_id)
+
+    # we should clear the context here, so we won't need to do another lambda
+    updateJson = _update_json()
+    put_file_contents(json.dumps(updateJson), "htmlcontext.json")
+    codepipeline.put_job_success_result(jobId=job_id)
